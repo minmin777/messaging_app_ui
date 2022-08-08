1257771e75f329aebe42dceb9d6384f35b279a7d
Yasmine Ayad <yasmineayad89@gmail.com>
2022-08-07 21:41:06 -0400

    remove key

--- messagingapp/messagingapp/settings.py
+++ messagingapp/messagingapp/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pho*s9fao_8+_0=d%t=zy34%vek_@+p69r)!*cl1w@%!a!jemu'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
